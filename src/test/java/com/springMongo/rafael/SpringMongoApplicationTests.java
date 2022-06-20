dfc3afc75ee66044ae920ff57b3350fc99239e68
rafa <ralfrr55@gmail.com>
2022-06-20 18:37:33 -0300

    2 commit

---
+++ src/test/java/com/springMongo/rafael/SpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springMongo.rafael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
