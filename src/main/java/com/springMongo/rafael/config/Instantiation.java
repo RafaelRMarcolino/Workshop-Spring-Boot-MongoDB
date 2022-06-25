ff49de136eed90afa9e0cc2f741b87f1e9b9480a
rafa <ralfrr55@gmail.com>
2022-06-25 12:44:01 -0300

    adicionando comentarios ao post

--- src/main/java/com/springMongo/rafael/config/Instantiation.java
+++ src/main/java/com/springMongo/rafael/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.springMongo.rafael.domain.Post;
 import com.springMongo.rafael.domain.User;
 import com.springMongo.rafael.dto.AuthorDTO;
+import com.springMongo.rafael.dto.CommentDTO;
 import com.springMongo.rafael.repositories.PostRepositories;
 import com.springMongo.rafael.repositories.UserRepositories;
 
@@ -43,6 +44,11 @@ public class Instantiation implements CommandLineRunner {
 		Post pos2 = new Post(null, sdf.parse("25/05/2000"), "Partiu minas", "aeeee", new AuthorDTO(us1));
 		Post pos3 = new Post(null, sdf.parse("25/05/2000"), "Partiu japao", "bora andar de barco", new AuthorDTO(us1));
 		
+		CommentDTO cdt1 = new CommentDTO("Boa viajem mano", sdf.parse("24/04/2001"), new AuthorDTO(us1));
+		CommentDTO cdt2 = new CommentDTO("Valeu falo", sdf.parse("24/04/2001"), new AuthorDTO(us2));
+		
+		pos1.getComments().add(cdt1);
+		pos2.getComments().add(cdt2);
 		
 		postRepo.saveAll(Arrays.asList(pos1, pos2, pos3));
 		
